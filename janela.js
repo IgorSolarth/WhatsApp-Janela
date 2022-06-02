bd280ee9962a31c4ac6dfbeb93f1a77055d39ca1
Igor Solarth <106268722+IgorSolarth@users.noreply.github.com>
2022-06-01 22:51:43 -0400

    atualização

---
+++ janela.js
@@ -0,0 +1,10 @@
+function abrirwp() {
+    let abrirwp = document.getElementById ('janela_wp');
+    
+    if(abrirwp.style.width == "0px"){
+        abrirwp.style.width = "170px";
+    }else {
+        abrirwp.style.width = "0px";
+    }
+}
+
